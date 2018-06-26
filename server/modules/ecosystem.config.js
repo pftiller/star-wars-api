6357d026b3ea21111e4c93b6d61b4fdc01404392
Paul Tiller <pftiller@gmail.com>
2018-06-26 00:32:36 -0500

    changing to master

--- server/modules/ecosystem.config.js
+++ server/modules/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       user : ' ubuntu',
       host : 'ec2-13-59-238-152.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/portfolio.pem',
-      ref  : 'origin/aws',
+      ref  : 'origin/master',
       repo : 'git@github.com:pftiller/weekend-challenge-5.git',
       path: '/home/ubuntu/weekend-challenge-5',
       'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
