d5a494459624a81b56cfe1a3efd47d012256d874
Paul Tiller <pftiller@gmail.com>
2018-06-25 21:48:00 -0500

    pm2

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const router = require('./routes/star-wars.router');
-const port = process.env.PORT || 5000;
+// const bodyParser = require('body-parser');
+// const router = require('./routes/star-wars.router');
+const port = 3000;
 /** -------- MONGOOSE CONNECTION --------**/
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
-var databaseUrl = 'mongodb://localhost:27017/mean-star-wars';
+// var databaseUrl = 'mongodb://localhost:27017/mean-star-wars';
 
-mongoose.connection.on('connected', function() {
-  console.log('mongoose connected to : ', databaseUrl);
-});
+// mongoose.connection.on('connected', function() {
+//   console.log('mongoose connected to : ', databaseUrl);
+// });
 
-mongoose.connection.on('error', function(err) {
-  console.log('mongoose connection error: ', err);
-});
+// mongoose.connection.on('error', function(err) {
+//   console.log('mongoose connection error: ', err);
+// });
 
-mongoose.connect(databaseUrl);
+// mongoose.connect(databaseUrl);
 
-module.exports = mongoose;
+// module.exports = mongoose;
 
-app.use(express.static('server/public/'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
-app.use('/router', router);
+// app.use(express.static('server/public/'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json()); 
+// app.use('/router', router);
 
 
 app.listen(port, function() {
