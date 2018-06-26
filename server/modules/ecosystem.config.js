009081ca56f29499624749880fba3d9235bec675
Paul Tiller <pftiller@gmail.com>
2018-06-25 23:45:11 -0500

    deleting unused

--- server/modules/ecosystem.config.js
+++ server/modules/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     production : {
       user : 'ubuntu',
       host : 'ec2-18-219-244-117.us-east-2.compute.amazonaws.com',
-      ref  : 'origin/master',
+      ref  : 'origin/aws',
       repo : 'git@github.com:pftiller/weekend-challenge-5.git',
       path: '/home/ubuntu/weekend-challenge-5',
       'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
