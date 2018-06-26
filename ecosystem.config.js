c6933e1d2dd57d0d03ba79037a90c8bce123f591
Paul Tiller <pftiller@gmail.com>
2018-06-25 22:07:09 -0500

    more modifications

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     apps: [{
       name: 'weekend-challenge-5',
-      script: './server.js'
+      script: 'server/server.js'
     }],
     deploy: {
       production: {
-        user: 'pftiller',
+        user: 'ubuntu',
         host: 'ec2-18-219-244-117.us-east-2.compute.amazonaws.com',
         key: '~/.ssh/portfolio.pem',
         ref: 'origin/aws',
