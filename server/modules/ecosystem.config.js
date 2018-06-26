58f566b774629665eb79d92f0daf1b1d949aee71
Paul Tiller <pftiller@gmail.com>
2018-06-25 23:43:19 -0500

    ecosystem in the right place

---
+++ server/modules/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [
+    {
+      name      : 'weekend-challenge-5',
+      script    : '../server/server.js'
+    }],
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ec2-18-219-244-117.us-east-2.compute.amazonaws.com',
+      ref  : 'origin/master',
+      repo : 'git@github.com:pftiller/weekend-challenge-5.git',
+      path: '/home/ubuntu/weekend-challenge-5',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+    }
+  }
+};
