5526c5dc86beb71979fd1104817de350d3de3ff6
Paul Tiller <pftiller@gmail.com>
2018-06-26 16:01:00 -0500

    switching

--- server/server.js
+++ server/server.js
@@ -3,22 +3,34 @@ const app = express();
 const bodyParser = require('body-parser');
 const router = require('./routes/star-wars.router');
 const port = 5418;
-/** -------- MONGOOSE CONNECTION --------**/
-var mongoose = require('mongoose');
+/** -------- ATLAS CONNECTION --------**/
+// var mongoose = require('mongoose');
 
-var databaseUrl = 'mongodb://localhost:27017/mean-star-wars';
+// var databaseUrl = 'mongodb://localhost:27017/mean-star-wars';
 
-mongoose.connection.on('connected', function() {
-  console.log('mongoose connected to : ', databaseUrl);
+// mongoose.connection.on('connected', function() {
+//   console.log('mongoose connected to : ', databaseUrl);
+// });
+
+// mongoose.connection.on('error', function(err) {
+//   console.log('mongoose connection error: ', err);
+// });
+
+// mongoose.connect(databaseUrl);
+
+// module.exports = mongoose;
+
+
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/test";
+MongoClient.connect(uri, function(err, client) {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
 });
 
-mongoose.connection.on('error', function(err) {
-  console.log('mongoose connection error: ', err);
-});
-
-mongoose.connect(databaseUrl);
-
-module.exports = mongoose;
+module.exports = MongoClient;
 
 app.use(express.static('server/public/'));
 app.use(bodyParser.urlencoded({ extended: true }));
