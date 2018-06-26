b193f2725aa14589bc20806630e970811495381f
Paul Tiller <pftiller@gmail.com>
2018-06-26 00:30:49 -0500

    tweaking ecosystem

--- server/modules/ecosystem.config.js
+++ server/modules/ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
     production : {
       user : ' ubuntu',
       host : 'ec2-13-59-238-152.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/portfolio.pem',
       ref  : 'origin/aws',
       repo : 'git@github.com:pftiller/weekend-challenge-5.git',
       path: '/home/ubuntu/weekend-challenge-5',
